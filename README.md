f6b5bb3f3a69f8f1800957527cee4deacf34f95e
dobermensch <dobermensch@gmail.com>
2024-06-12 23:35:45 +0800

    mod: README && rm hh contracts

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-wagmi`](https://github.com/wevm/wagmi/tree/main/packages/create-wagmi).
+# Posley Swap Frontend
+
+## Getting started
+
+1. Have contracts from backend repo running and deployed to Hardhat node
+2. Clone this repo
+3. `cd` into this repo & run `npm i` (have node installed)
+4. Change contract addresses to hardhat deployed addresses in `Contracts` variable in `/src/app/constants.ts`
+5. Run `npm run dev`
+6. Head to `http://localhost:3000`
+7. Hire me
--- src/app/constants.ts
+++ src/app/constants.ts
@@ -3,19 +3,14 @@ import { ContractsType } from './types';
 
 export const Contracts: ContractsType = {
   Ethereum: undefined,
-  Hardhat: {
-    MockGoldAddress: '0x610178dA211FEF7D417bC0e6FeD39F05609AD788',
-    MockPythAddress: '0xB7f8BC63BbcaD18155201308C8f3540b07f84F5e',
-    MockUSDCAddress: '0xA51c1fc2f0D1a1b8494Ed1FE312d7C3a78Ed91C0',
-    OracleAmmAddress: '0x0DCd1Bf9A1b36cE34237eEaFef220932846BCD82',
-  },
+  Hardhat: undefined,
   Sepolia: undefined,
 };
 export const DefaultDecimals = 18;
 export const MockGoldPriceID =
   '0x9d4294bbcd1174d6f2003ec365831e64cc31d9f6f15a2b85399db8d5000960f6';
 export const MockPythConnection = {
-  getPriceFeedsUpdateData: function (pairPriceIds: string[]) {
+  getPriceFeedsUpdateData: function (_: string[]) {
     return [];
   },
 };
