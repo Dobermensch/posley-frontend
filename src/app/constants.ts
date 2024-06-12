f7562dd5332a508a03adbfc1d0f055f0cd0fb8b6
dobermensch <dobermensch@gmail.com>
2024-06-12 23:56:30 +0800

    mod: pyth network connection from aptos

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -17,5 +17,5 @@ export const MockPythConnection = {
 export const MockUSDCPriceID =
   '0x9d4294bbcd1174d6f2003ec365831e64cc31d9f6f15a2b85399db8d5000960f5';
 export const PythConnection = new EvmPriceServiceConnection(
-  'https://xc-testnet.pyth.network'
+  'https://hermes.pyth.network'
 );
