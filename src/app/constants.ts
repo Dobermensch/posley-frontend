4ed13736a5cfda4b85acae7fbafc69ccd6dddeb8
dobermensch <dobermensch@gmail.com>
2024-06-13 10:57:47 +0800

    add: sepolia contract addresses

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -3,7 +3,12 @@ import { ContractsType } from './types';
 
 export const Contracts: ContractsType = {
   Hardhat: undefined,
-  Sepolia: undefined,
+  Sepolia: {
+    MockGoldAddress: '0x0313f7d09C54A9cA9D8bC475B06FB0feBE7c53Db',
+    MockPythAddress: '0x7DF359822AfFf1d01dD0e724816b4958Bf9e4512',
+    MockUSDCAddress: '0xF6E478F2d438021516B5BDA60984C59E0d7534D0',
+    OracleAmmAddress: '0x9A3Bf547A67B129fE93ed5a9F54EaaB9CaCf0664',
+  },
 };
 export const DefaultDecimals = 18;
 export const MockGoldPriceID =
